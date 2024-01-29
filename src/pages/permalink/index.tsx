1d0514e35e824c6d893285faedb9cb75e29f66af
Harish V <harish@open.gov.sg>
2024-01-29 17:34:19 +0800

    fix styles

--- src/pages/permalink/index.tsx
+++ src/pages/permalink/index.tsx
@@ -1,5 +1,5 @@
 import { RenderEngine } from "@isomerpages/isomer-components";
-import schema from "../../schema/schema";
+import schema from "../../schema/permalink/schema";
 
 export default function Page() {
   return (
