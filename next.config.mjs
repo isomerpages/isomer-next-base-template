0e193834d231dc1ace4bd9b7a99537d8de89c25a
Harish V <harish@open.gov.sg>
2024-02-12 15:42:11 +0800

    update: next build mode

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
+  indexExtensions: ["index.tsx", "index.ts", "index.jsx", "index.js"],
 };
 
 export default nextConfig;
