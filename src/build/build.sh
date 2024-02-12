ecfee5ea241fa4fcf26db035e728e436bbc259dd
Harish V <harish@open.gov.sg>
2024-02-12 17:37:45 +0800

    update: build script

--- src/build/build.sh
+++ src/build/build.sh
@@ -6,15 +6,8 @@ echo "Build started..."
 # Download package.json and package-lock.json files from central repo #
 #######################################################################
 
-curl https://raw.githubusercontent.com/isomerpages/isomer-template/master/build/package.json -o package.json
-curl https://raw.githubusercontent.com/isomerpages/isomer-template/master/build/package-lock.json -o package-lock.json
-
-#######################################################################
-# Generate sitemap.json and search index #
-#######################################################################
-
-curl https://raw.githubusercontent.com/isomerpages/isomer-next-build/main/scripts/generate-sitemap.js -o src/scripts/generate-sitemap.js
-curl https://raw.githubusercontent.com/isomerpages/isomer-next-build/main/scripts/generate-search-index.js -o src/scripts/generate-search-index.js
+curl https://raw.githubusercontent.com/isomerpages/isomer-next-base-template/main/package.json -o package.json
+curl https://raw.githubusercontent.com/isomerpages/isomer-next-base-template/main/package-lock.json -o package-lock.json
 
 #######################
 # Install NPM modules #
@@ -22,8 +15,29 @@ curl https://raw.githubusercontent.com/isomerpages/isomer-next-build/main/script
 
 npm install
 
-#####################
-# Build the website #
-#####################
+#######################################################################
+# Generate sitemap.json and search index #
+#######################################################################
+mkdir -p src/scripts/
 
-npm run build
+curl https://raw.githubusercontent.com/isomerpages/isomer-next-build/main/scripts/generate-sitemap.js -o src/scripts/generate-sitemap.js
+curl https://raw.githubusercontent.com/isomerpages/isomer-next-build/main/scripts/generate-search-index.js -o src/scripts/generate-search-index.js
+
+node src/scripts/generate-sitemap.js
+
+echo "Sitemap generated"
+
+node src/scripts/generate-search-index.js
+
+echo "Search index generated"
+
+#######################################################################
+# Copy to public folder #
+#######################################################################
+
+cp -v src/sitemap.json public/
+cp -v src/searchIndex.json public/
+
+echo "Copied sitemap and search index to public folder"
+
+
