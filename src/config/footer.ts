0a10dc4134893beb4ae8c9ed931cdcf87029a886
Harish V <harish@open.gov.sg>
2024-02-12 17:09:24 +0800

    add default navbar and footer

---
+++ src/config/footer.ts
@@ -0,0 +1,13 @@
+export const Footer = {
+  id: "Footer",
+  components: [
+    {
+      id: "Footer",
+      props: {
+        agencyName: "Isomer Next",
+        lastUpdated: "2024-01-28",
+        items: [],
+      },
+    },
+  ],
+};
---
+++ src/config/navbar.ts
@@ -0,0 +1,15 @@
+export const Navbar = {
+  id: "Navbar",
+  components: [
+    {
+      id: "Navbar",
+      props: {
+        logo: {
+          url: "https://www.isomer.gov.sg/images/isomer-logo.svg",
+          alt: "Isomer logo",
+        },
+        links: [],
+      },
+    },
+  ],
+};
