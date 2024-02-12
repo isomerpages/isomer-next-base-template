9a7794ce1b2e096a2d9f3d4390322341fd0d410b
Harish V <harish@open.gov.sg>
2024-02-12 22:55:02 +0800

    minor change

--- src/build/build.sh
+++ src/build/build.sh
@@ -2,6 +2,6 @@
 
 echo "Prebuild started..."
 mkdir -p src/scripts/
-curl https://raw.githubusercontent.com/isomerpages/isomer-next-build/main/scripts/build.sh -o src/scripts/build.sh
-sh src/scripts/build.sh
+curl https://raw.githubusercontent.com/isomerpages/isomer-next-build/main/scripts/build.sh -o src/scripts/runBuild.sh
+sh src/scripts/runBuild.sh
 echo "Prebuild completed"
