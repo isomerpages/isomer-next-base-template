8824a4722cdaaa30638165147f3f4c0065611bcd
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-03-22 14:51:36 +0800

    feat: enable trailingSlash for prettier URLs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   output: "export",
   reactStrictMode: true,
   indexExtensions: ["index.tsx", "index.ts", "index.jsx", "index.js"],
+  trailingSlash: true,
 };
 
 export default nextConfig;
