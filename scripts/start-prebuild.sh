7b3f6c59a1f11640a63ccf8c16bcf5b2c02214aa
Harish V <harish@open.gov.sg>
2024-04-28 18:33:37 +0800

    fix: source for prebuild script

--- scripts/start-prebuild.sh
+++ scripts/start-prebuild.sh
@@ -2,5 +2,5 @@
 
 set -x
 
-curl https://raw.githubusercontent.com/isomerpages/isomer-next-base-template/main/scripts/preBuild.sh -o scripts/preBuild.sh
+curl https://raw.githubusercontent.com/isomerpages/isomer-next-build/main/scripts/preBuild.sh -o scripts/preBuild.sh
 sh scripts/preBuild.sh
